12d85377bdfe7229c5ab02fb42796018d66584ea
Nick Clifford <nicholas.clifford@annkissam.com>
2019-05-21 15:17:28 -0400

    bump version to 0.6.0

--- lib/dalliance/version.rb
+++ lib/dalliance/version.rb
@@ -1,8 +1,8 @@
 module Dalliance
   module VERSION
     MAJOR = 0
-    MINOR = 5
-    TINY  = 1
+    MINOR = 6
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
